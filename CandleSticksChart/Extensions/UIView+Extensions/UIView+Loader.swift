097de8c814b9a989f775225383f298410b3d55e4
Jeddawy <ielgeddawy@gmail.com>
2022-11-01 04:41:56 +0200

    [ADD] UIView Extension to show and hide Loader

---
+++ CandleSticksChart/Extensions/UIView+Extensions/UIView+Loader.swift
@@ -0,0 +1,38 @@
+//
+//  UIView+Loader.swift
+//  CandleSticksChart
+//
+//  Created by Geddawy on 01/11/2022.
+//
+
+import UIKit
+extension UIView {
+    func showLoader() {
+        let activityIndicator = setupActivityIndicator()
+        self.addSubview(activityIndicator)
+    }
+    
+    func hideLoader() {
+        if let activityIndicator = viewWithTag(333) {
+            activityIndicator.removeFromSuperview()
+        }
+    }
+    
+    private func setupActivityIndicator() -> UIActivityIndicatorView {
+        let activityIndicator = UIActivityIndicatorView()
+        activityIndicator.frame = self.bounds
+        activityIndicator.center = self.center
+        activityIndicator.style = .gray
+        activityIndicator.tag = 333
+        return activityIndicator
+    }
+    
+    func roundCorner(_ corners: UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        self.layer.mask = mask
+        self.layer.masksToBounds = false
+    }
+    
+}
