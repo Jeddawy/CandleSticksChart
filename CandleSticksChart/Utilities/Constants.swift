591f79ef7688d84747cae16a2c207eb193bb4fec
Jeddawy <ielgeddawy@gmail.com>
2022-11-01 04:07:31 +0200

    [Setup] project base

---
+++ CandleSticksChart/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  CandleSticksChart
+//
+//  Created by Geddawy on 01/11/2022.
+//
+
+import Foundation
+
+struct URLs{
+    static let url = "https://fapi.binance.com/fapi/v1/klines"
+}
